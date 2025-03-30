c84963e741765949fe2f9af90b25c82ac447d92f
zqpaperpiano <ziqing.ong.2021@socsc.smu.edu.sg>
2025-03-30 15:55:57 +0800

    complpelted navbar with routing and current tab indication

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from '../../assets/logo-no-animation.png'
 import { Github, Linkedin } from 'lucide-react';
+import { useLocation, useNavigate } from "react-router";
 
 
 
 const NavBar: React.FC = () => {
-
+    const navigate = useNavigate();
+    const location = useLocation();
 
     return(
         <div className="h-full w-full flex justify-center items-center">
@@ -15,22 +17,27 @@ const NavBar: React.FC = () => {
                     <div className="h-2/3 aspect-square">
                         <img src={logo} alt="logo" className="h-full w-full object-cover" />
                     </div>
-                    <div className="h-full w-auto flex items-center justify-center text-white text-xl"> ZQPAPERPIANO </div>
-                    <div className="absolute h-full w-full main-bg opacity-30 hover:cursor-pointer hover:opacity-0"></div>
+                    <div className="h-full w-auto flex items-center justify-center text-white text-xl font-medium"> ZQPAPERPIANO </div>
+                    <div 
+                    onClick={() => navigate('/')}
+                    className="absolute h-full w-full main-bg opacity-30 hover:cursor-pointer hover:opacity-0"></div>
                     
                 </div>
 
                 {/* main part of navbar */}
-                <div className="h-full w-1/2 flex items-center justify-center gap-4 text-[#d5d7d8]">
-                    <div>HOME</div>
-                    <div>SKILLS</div>
-                    <div>PROJECTS</div>
-                    <div>ZIQING</div>
+                <div className="h-full w-1/2 flex items-center justify-center gap-4 font-medium">
+                    <div className={`hover:cursor-pointer ${location.pathname === '/' ? 'selected-txt underline underline-offset-[3px] decoration-[2px] decoration-solid' : 'hover:text-white text-[#d5d7d8]'}`} onClick={() => navigate('/')}>HOME</div>
+                    <div className={`hover:cursor-pointer ${location.pathname === '/skills' ? 'selected-txt underline underline-offset-[3px] decoration-[2px] decoration-solid' : 'hover:text-white text-[#d5d7d8]'}`} onClick={() => navigate('/skills')}>SKILLS</div>
+                    <div className={`hover:cursor-pointer ${location.pathname === '/projects' ? 'selected-txt underline underline-offset-[3px] decoration-[2px] decoration-solid' : 'hover:text-white text-[#d5d7d8]'}`} onClick={() => navigate('/projects')}>PROJECTS</div>
+                    <div className={`hover:cursor-pointer ${location.pathname === '/ziqing' ? 'selected-txt underline underline-offset-[3px] decoration-[2px] decoration-solid' : 'hover:text-white text-[#d5d7d8]'}`} onClick={() => navigate('/ziqing')}>ZIQING</div>
                 </div>
 
                 <div className="h-full w-1/4 flex items-center justify-center gap-2">
-                    <Github size={20} className="text-[#8a9198] hover:cursor-pointer hover:text-white" strokeWidth={1.25} />
-                    <Linkedin size={20} className="text-[#8a9198] hover:cursor-pointer hover:text-white" strokeWidth={1.25} />
+                <a href="https://github.com/zqpaperpiano"><Github 
+                    size={20} className="text-[#8a9198] hover:cursor-pointer hover:text-white" strokeWidth={1.25} /></a>
+                    
+                <a href="https://www.linkedin.com/in/zi-qing-ong-920449233/"><Linkedin 
+                    size={20} className="text-[#8a9198] hover:cursor-pointer hover:text-white" strokeWidth={1.25} /></a>
                 </div>
             
             </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App.tsx'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
